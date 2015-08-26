79af7e3ca45568a1cdd62c1a37d07b62b5acede0
billryan <yuanbin2014@gmail.com>
2015-08-26 13:55:31 +0800

    remove extra case class

--- akka_messaging/src/main/scala/me/yuanbin/akkanotes/protocols/StudentProtocol.scala
+++ akka_messaging/src/main/scala/me/yuanbin/akkanotes/protocols/StudentProtocol.scala
@@ -2,13 +2,6 @@ package me.yuanbin.akkanotes.protocols
 
 object StudentProtocol {
   /*
-   * the Driver App issues this signal to ask the
-   * Student actor to send message to Teacher Actor
-   *
-   */
-  case class InitSignal()
-
-  /*
    * The Student sends this message to request for a Quotation
    *
    */
